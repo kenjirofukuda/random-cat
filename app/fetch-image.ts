6c6c6beb1be2078dddcdcb9d5f66efc1051d03a8
Kenjiro Fukuda <kenjirofukuda@gmail.com>
2025-12-02 15:57:43 +0900

    add strict responce check

--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -9,6 +9,43 @@ export async function fetchImage(): Promise<Image> {
   //                                ^^^^^^^^^^^^^^ 型注釈を追加
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
+  if (! isImageArray(images)) {
+    throw new Error("取得したデータが正しくありません");
+  }
+  if (! isImage(images[0])) {
+    throw new Error("取得したデータが空です");
+  }
   console.log("fetchImages: 画像情報を取得しました。", images);
   return images[0]; // 画像情報の配列から最初の要素を返す。
+}
+
+// Image型の配列であるかチェックする関数
+function isImageArray(value: unknown): value is Image[] {
+  // valueが配列であること
+  if (! Array.isArray(value)) {
+    return false;
+  }
+  // 配列の要素すべてがImage型であること
+  if (! value.every(isImage)) {
+    return false;
+  }
+
+  return true;
+}
+
+// Image型であるかをチェックする関数
+function isImage(value: unknown): value is Image {
+  // valueがオブジェクトであること
+  if (typeof value !== "object" || value === null) {
+    return false;
+  }
+  // valueにurlフィールドがあること
+  if (! ("url" in value)) {
+    return false;
+  }
+  // urlフィールドが文字列であること
+  if (typeof (value as Image).url !== "string") {
+    return false;
+  }
+  return true;
 }
