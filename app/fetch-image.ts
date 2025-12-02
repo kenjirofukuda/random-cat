faefb4b6549f6f6dbcfa567aad0b3499be939212
Kenjiro Fukuda <kenjirofukuda@gmail.com>
2025-12-02 14:40:52 +0900

    add Image type

--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -1,5 +1,12 @@
+// 画像情報の型定義
+type Image = {
+  url: string;
+};
+
+
 // APIから画像を取得する関数
-export async function fetchImage() {
+export async function fetchImage(): Promise<Image> {
+  //                                ^^^^^^^^^^^^^^ 型注釈を追加
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
   console.log("fetchImages: 画像情報を取得しました。", images);
